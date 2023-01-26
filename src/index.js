a5d3fa8dd9e534bd30b29541687e067d916fac4b
PRIYAsingh13 <ps597934@gmail.com>
2023-01-26 11:19:50 +0530

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const name = "priya"
+const lname = "singh"
+const currDate = new Date().getDate();
+const currDate1 = new Date().toLocaleDateString()
+const currTime = new Date().toLocaleTimeString()
+const img1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNaI3k3GEVnCEXd54ISam3Ix7uwWZtIj1nHg&usqp=CAU"
+const img2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNaI3k3GEVnCEXd54ISam3Ix7uwWZtIj1nHg&usqp=CAU"
+const img3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNaI3k3GEVnCEXd54ISam3Ix7uwWZtIj1nHg&usqp=CAU"
+const links = "https://www.google.co.in/"
+
+
+
+ReactDOM.render(
+  <>
+    <h1>priya</h1>
+    <p>List of 5 series</p>
+    <ol>
+      <li>Dark</li>
+      <li>two</li>
+      <li>three</li>
+      <li>four</li>
+      <li>five</li>
+
+    </ol>
+    <h1> {name} is the best</h1>
+    <h1>{`my name is ${name} ${lname}`}</h1>
+    <p>Current Date is = {currDate}</p>
+    <p>Current Date is = {currDate1}</p>
+    <p>Current Time is = {currTime}</p>
+
+    <h1 contentEditable="true">i am web developer</h1>
+
+    <img src={img1} alt="flag" />
+    <img src={img2} alt="flag" />
+    <img src={img3} alt="flag" />
+
+    <a href={links} target="_google" >go to google</a>
+
+
+
+
+  </>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
